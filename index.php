c65a9542c44dee5da9c57cd9594c65b035c4f088
Wisertown <aaron.wise253@gmail.com>
2016-08-01 20:38:32 -0400

    finished template form

--- index.php
+++ index.php
@@ -14,39 +14,34 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+	<script>
+	/*$('#vot').change(function(){
 
+		$(this).animate({
+   width: '150%'
+}, { duration: 200, queue: false });
+		$(this).animate({
+	 width: '70%'		
+}, {duration: 200, queue: false });
+	});*/
+
+	</script>
 </head>
 <body id="body">
-	<ANY
-    dir-paginate="expression | itemsPerPage: (int|expression) [: paginationId (string literal)]"
-    [current-page=""]
-    [pagination-id=""]
-    [total-items=""]>
-    
-    </ANY>
 
-	<div class="container">
+
+<div class="container">
 	<div class="col-md-12 col-sm-12 col-xs-12 text-center">
 		<h1>My First Car</h1>
 		<!--<p>Tell us about your first car, users get to vote on the coolest first car</p>-->
 		<p>Vote on which first car is the coolest!</p>
-		<p>Vote as many times as you want! Nobody cares!</p>
+		<p>Vote as many times as you want!</p>
 	</div>
 
 	<br>
 	<div class="col-md-12 col-sm-12 col-xs-12">
 
 	<div class="container wrapper" ng-controller="DbController">
-		
-		<dir-pagination-controls
-    [max-size=""]
-    [direction-links=""]
-    [boundary-links=""]
-    [on-page-change=""]
-    [pagination-id=""]
-    [template-url=""]
-    [auto-hide=""]>
-    </dir-pagination-controls>
 		<div class="col-md-12 col-sm-12 col-xs-12 text-center">
 			<div class='alert alert-default input-group search-box'>
 				<span>
@@ -55,8 +50,16 @@
 				</span>
 			</div>
 		</div>
-		<br>
-		<div class="col-md-12 col-sm-12 col-xs-12">
+
+<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+	<button class="btn btn-warning" ng-click="toggleShow()">Add Your First Car</button>
+</div>
+
+		<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+			<div ng-include src="'templates/newForm.html'"></div>
+		</div>
+
+	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div ng-repeat="detail in details| filter:query |orderBy: 'year'">
 			<div class="hold">
 				<div class="img-holder"><img src="{{detail.photoURL}}" alt="car" class="imgh"></div>
@@ -71,16 +74,15 @@
 					
 					<p id='comms'>Comments:{{detail.comments}}</p>
 					</div>
-					<span class="spanm">VOTES: {{detail.vote}}</span>
+					<span class="spanm" id="vot">VOTES: {{detail.vote}}</span>
 				</div>
 				<div class="btn-hold">
 					<button ng-click="insertInfo(detail.id)"class="btn btn-primary">VOTE</button>
 				</div>
 			</div>
-
 		</div>
 		</div>
-	</div>
+		</div>
 	</div>
 </div>
 </body>
--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-angular.module('myApp', ['angularUtils.directives.dirPagination']);
-
+/*angular.module('carApp', ['angularUtils.directives.dirPagination']);
+angular.module('carApp', [
+    'angularFileUpload'
+]);*/
 var carApp = angular.module('carApp',[]);
 carApp.controller("DbController",['$scope','$http', function($scope,$http){
 
 // Function to get employee details from the database
 getInfo();
+
 function getInfo(){
 // Sending request to EmpDetails.php files
 $http.get('cardetails.php').success(function(data){
@@ -16,17 +19,34 @@ $scope.details = data;
 $scope.insertInfo = function(id){
 
 $http.post('insertvote.php',{"id":id}).success(function(data){
-console.log(data);
+
 if (data == true) {
+
 getInfo();
-// Hide details insertion form
+$('#vot').addClass("intro");
 
 }
+
+// Hide details insertion form
 });
 }
 
+$scope.insertCar = function(carInfo){
+	$http.post('insertCar.php', {})
+}
 
+
+$scope.toggleShow = function(){
+	$('#carForm').slideToggle();
+}
+
+$scope.show_form = true;
+
+$scope.currentUser = {};
+$scope.editInfo = function(info){
+$scope.currentUser = info;
+//$('#empForm').slideUp();
+$('#carForm').slideToggle();
+}
 }]);
-
-
 
